3f1be36e1ea3950392d210b563def7e7a6c05702
Diego Andres Elgueta Casanova <diegoelgueta2001@gmail.com>
2024-12-18 11:33:36 -0300

    Improved App Graphics

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::FontOptions (32.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setFont (juce::FontOptions (48.0f));
+    g.setColour (juce::Colours::red);
+    g.drawText ("Hello World by Diego!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
